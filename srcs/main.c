c3750ff6dfef9e53bdd1b97d2f5f225919b5004f
petja <petja@debian.vm>
2022-01-06 07:54:27 +0200

    changed some function and variable types in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,32 @@
 /*   By: pniva <pniva@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 10:25:16 by pniva             #+#    #+#             */
-/*   Updated: 2022/01/05 10:33:52 by pniva            ###   ########.fr       */
+/*   Updated: 2022/01/06 07:53:58 by pniva            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(int argc, char *argv[])
 {
-	char	**tetriminos;
+	t_etris	*tetri_first;
 	char	**solution;
 	
 	if (!argc == 2)
 	{
 		ft_putstr_fd("Please give filename", 1);
 	}
-	tetriminos = read_tetriminos(argv[1]);
-	if (!validate_tetriminos(tetriminos))
+	tetri_first = read_tetriminos(argv[1]);
+	if (!validate_tetriminos(tetri_first))
 	{
 			ft_putstr_fd("error", 1);
 			return (0);
 	}
-	solution = solve(tetriminos);
+	solution = solve(tetri_first);
 	if (!solution)
 	{
 		ft_putstr_fd("error", 1);
 		return (0);
 	}
 	print(solution);
-	free_memory(tetriminos, solution);
+	free_memory(tetri_first, solution);
 	return (0);
 }
