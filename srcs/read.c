639060fcb45c9434777ae16a2d5de33418331a0a
Bengisu Kandemir <bkandemi@student.hive.fi>
2022-01-12 17:31:44 +0200

    read.c passed norminette

--- srcs/read.c
+++ srcs/read.c
@@ -6,7 +6,7 @@
 /*   By: bkandemi <bkandemi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/05 12:54:14 by pniva             #+#    #+#             */
-/*   Updated: 2022/01/12 17:28:34 by bkandemi         ###   ########.fr       */
+/*   Updated: 2022/01/12 17:31:19 by bkandemi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,32 +30,6 @@ t_etris	*from_file_to_list(char *filename)
 	return (NULL);
 }
 
-
-//TODO Too long, split or change?
-
-
-/*
-** Reads the next line from the file. If line exists and if strlen is = 0, checks if next line
-** is line of a valid mino. If strlen of line was != 0, checks if that line
-** is of a valid mino.
-*/
-// TODO error check for no new line between minos
-/*int	there_is_next_mino(int fd, char **line)
-{
-	if (ft_get_next_line(fd, line))
-	{
-		if (ft_strlen(*line) == 0)
-		{
-			if (ft_get_next_line(fd, line))
-				if (check_line(*line))
-					return (TRUE);
-		}
-		else if (check_line(*line))
-			return (TRUE);
-	}
-	return (FALSE);
-}*/
-
 int	check_line(char *line)
 {
 	int	i;
@@ -72,25 +46,6 @@ int	check_line(char *line)
 	return (TRUE);
 }
 
-/*t_etris	*get_next_mino(int fd, char **line)
-{
-	t_etris	*mino;
-	char	yx[4][4];
-	int		i;
-
-	i = 0;
-	while (i < 4)
-	{
-		ft_strcpy(yx[i], *line);
-		if (!ft_get_next_line(fd, line) && i != 3)
-			return (NULL);
-		++i;
-	}
-	mino = create_mino(yx);
-	return (mino);
-}*/
-
-//TODO: rename align and convert?
 t_etris	*create_mino(char yx[4][4])
 {
 	t_etris	*mino;
@@ -109,11 +64,11 @@ t_etris	*create_mino(char yx[4][4])
 	return (mino);
 }
 
-int	read_mino(int fd, t_etris **head) 
+int	read_mino(int fd, t_etris **head)
 {
-	char *line;
+	char	*line;
 	char	yx[4][4];
-	int	i;
+	int		i;
 
 	i = 1;
 	while (ft_get_next_line(fd, &line) > 0)
@@ -141,6 +96,7 @@ int	read_mino(int fd, t_etris **head)
 void	add_mino_to_list(t_etris **head, t_etris *new)
 {
 	t_etris	*tmp;
+
 	if (*head == NULL)
 	{
 		*head = new;
@@ -155,4 +111,3 @@ void	add_mino_to_list(t_etris **head, t_etris *new)
 		new->c = tmp->c + 1;
 	}
 }
-
